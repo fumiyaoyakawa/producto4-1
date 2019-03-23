bfc9a32f46bd11a7f13b0e0536253d3ef5416e85
Paulo Cesar Loayza Carrasco <paulocesarloayza@upsa.edu.bo>
2019-03-23 13:38:35 -0400

    api con azure

---
+++ productoBack/productoAPI/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace productoAPI.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<productoAPI.Models.Product> Products { get; set; }
+    }
+}
